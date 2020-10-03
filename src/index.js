54d07fabd7fca49fefdc427cbea043be56139ced
Yevheniia-B <evgenijar.work@gmail.com>
2020-10-03 16:17:08 +0300

    feat: typical-arrays-problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
+exports.min = function min(array) {
+    if (Array.isArray(array) && array.length > 0) {
+        return Math.min(...array);
+    } else {
+        return 0;
+    }
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (Array.isArray(array) && array.length > 0) {
+        return Math.max(...array);
+    } else {
+        return 0;
+    }
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (Array.isArray(array) && array.length > 0) {
+        let i = 0;
+        let value = 0;
+        while (i < array.length) {
+            value = value + array[i];
+            console.log(value);
+            i = i + 1;
+        }
+        return value / array.length;
+    } else {
+        return 0;
+    }
+};
